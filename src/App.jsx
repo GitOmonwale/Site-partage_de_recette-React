4077f1ee91dd6096255d757f005ee43efe011038
GitOmonwalé <inesagbozo@gmail>
2024-07-24 13:09:31 +0100

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react';
 
-import Homepage3 from './pages/Homepage3/Homepage3';
-import Homepage4 from './pages/Homepage4/Homepage4';
-import Homepage5 from './pages/Homepage5/Homepage5';
 function App() {
   return (
     <>
- <Homepage5></Homepage5>
+ wxcvbgghhhhh
     </>
   )
 }
